6f6e59132cb1bbc330340fbc0ead4fbec47fbd65
X_zzzzz <195286381@qq.com>
2020-04-20 12:55:34 +0800

    Create BinaryFile.java

---
+++ src/net/mindview/util/BinaryFile.java
@@ -0,0 +1,8 @@
+package net.mindview.util;
+import java.io;
+
+public class BinaryFile {
+    public static byte[] read(File bFile) throws IOException {
+        BufferedInputSteam bf = new BufferedInputStream(mew)
+    }
+}
